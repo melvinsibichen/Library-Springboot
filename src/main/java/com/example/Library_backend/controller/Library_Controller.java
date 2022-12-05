7182d1a140c7edbe6c0f7f87581632e49d2be84f
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-05 22:33:10 +0530

    development updaed

--- src/main/java/com/example/Library_backend/controller/Library_Controller.java
+++ src/main/java/com/example/Library_backend/controller/Library_Controller.java
@@ -1,5 +1,6 @@
 package com.example.Library_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,53 @@ public class Library_Controller {
     {
         return "adminlogin";
     }
+
+    @GetMapping("/view")
+    public String View()
+    {
+        return "Viewpage";
+    }
+
+    @PostMapping("/edit")
+    public String Edit()
+    {
+        return "Editpage";
+    }
+
+    @PostMapping("/search")
+    public String Search()
+    {
+        return "Searchpage";
+    }
+
+    @PostMapping("/delete")
+    public String Delete()
+    {
+        return "Deletepage";
+    }
+
+    @PostMapping("/userlogin")
+    public String UserLogin()
+    {
+        return "Userlogin page";
+    }
+
+    @PostMapping("/issue")
+    public String Issue()
+    {
+        return "Issuepage";
+    }
+
+    @PostMapping("/usersignup")
+    public String UserSignUp()
+    {
+        return "Usersignuppage";
+    }
+
+    @PostMapping("/entry")
+    public String Entry()
+    {
+        return "Entrypage";
+    }
+
 }
